316a4f625303e8471f1d497b8c3948decbf6b718
isma91 <ismaydogmus@gmail.com>
2016-07-12 15:33:12 +0200

    adding empty test pydio view file

---
+++ view/test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="A tool to test your Pydio or a new one" />
+    <title>Welcome to Pydio-test !!</title>
+    <?php include "media.html" ?>
+    <script src="media/js/home_page.js"></script>
+</head>
+<body>
+    <div class="container" id="the_body">
+        <div class="row mui-panel">
+            <h1 class="title">Welcome to Pydio-test !!</h1>
+            <h2 class="title">You can test your Pydio here !!</h2>
+        </div>
+        <div class="row">
+        </div>
+    </div>
+</body>
+</html>
