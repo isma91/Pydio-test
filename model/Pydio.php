b85363d3aecccba03aa8c1b10b032cad0a44cd25
isma91 <ismaydogmus@gmail.com>
2016-07-12 15:32:53 +0200

    adding empty pydio model

---
+++ model/Pydio.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Pydio.php
+*
+* The model of Pydio
+*
+* PHP 7.0.4-7ubuntu2.1 (cli) ( NTS )
+*
+* @category Model
+* @package  Model
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+namespace model;
+class Pydio
+{
+}
