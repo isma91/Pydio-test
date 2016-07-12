56348425d2e060c9f60710415153d7a5b39332a2
isma91 <ismaydogmus@gmail.com>
2016-07-12 11:28:03 +0200

    adding api controller

---
+++ public_api/index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* Index.php
+*
+* All ajax request go here and be sended to different Controller
+*
+* PHP 7.0.4-7ubuntu2.1 (cli) ( NTS )
+*
+* @category Controller
+* @package  Controller
+* @author   isma91 <ismaydogmus@gmail.com>
+* @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+*/
+session_start();
+require '../autoload.php';
+switch ($_POST["action"]) {
+}
