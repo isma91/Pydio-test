19346f485c57db8fedd101ddf6a529f53fd330f8
isma91 <ismaydogmus@gmail.com>
2016-07-12 18:23:04 +0200

    adding the pydio url

--- media/js/home_page.js
+++ media/js/home_page.js
@@ -4,6 +4,7 @@
 $(document).ready(function () {
     "use strict";
     var path_to_ajax = "public_api/index.php";
+    var div_error;
     function press_enter(selector, go_function) {
         $(document).on("keyup", selector, function (event) {
             if (event.keyCode === 13) {
@@ -12,29 +13,38 @@ $(document).ready(function () {
         });
     }
     function take_pydio_path() {
+        div_error = "";
+        $("#div_error").html("");
         if ($.trim($("#pydio_path").val()) === "") {
-            Materialize.toast("<p class='alert-failed'>Empty Pydio Path !!<p>", 1000, "rounded alert-failed");
-            return false;
+            div_error = div_error + "<p>Empty Pydio Path !!</p>";
         }
-        $.post(path_to_ajax, {action: "add_pydio_path", pydio_path: $.trim($("#pydio_path").val())}, function (data, textStatus) {
-            if (textStatus === "success") {
-                data = JSON.parse(data);
-                if (data.error === null) {
-                    Materialize.toast("<p class='alert-success'>Pydio path added successfully !!<p>", 750, "rounded alert-success");
-                    setTimeout(function () {
-                        window.location = "?page=test";
-                    }, 1000);
+        if ($.trim($("#pydio_server_url").val()) === "") {
+            div_error = div_error + "<p>Empty Pydio URL !!</p>";
+        }
+        if (div_error !== "") {
+            $("#div_error").html(div_error);
+        } else {
+            $.post(path_to_ajax, {action: "add_pydio_path", pydio_path: $.trim($("#pydio_path").val()), pydio_url: $.trim($("#pydio_server_url").val())}, function (data, textStatus) {
+                if (textStatus === "success") {
+                    data = JSON.parse(data);
+                    if (data.error === null) {
+                        Materialize.toast("<p class='alert-success'>Pydio path added successfully !!<p>", 750, "rounded alert-success");
+                        setTimeout(function () {
+                            window.location = "?page=test";
+                        }, 1000);
+                    } else {
+                        Materialize.toast("<p class='alert-failed'>" + data.error + "<p>", 3000, "rounded alert-failed");
+                    }
                 } else {
-                    Materialize.toast("<p class='alert-failed'>" + data.error + "<p>", 3000, "rounded alert-failed");
+                    Materialize.toast("<p class='alert-failed'>Something is wrong while we try to add your Pydio path !!<p>", 3000, "rounded alert-failed");
                 }
-            } else {
-                Materialize.toast("<p class='alert-failed'>Something is wrong while we try to add your Pydio path !!<p>", 3000, "rounded alert-failed");
-            }
-        });
+            });
+        }
     }
     $(document).on("click", "#validate_pydio_path", function (event) {
         event.preventDefault();
         take_pydio_path();
     });
     press_enter("#pydio_path", take_pydio_path);
+    press_enter("#pydio_server_url", take_pydio_path);
 });
