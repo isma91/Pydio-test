cdf45c53b555913e224a975425b5c9d65e45e2b1
isma91 <ismaydogmus@gmail.com>
2016-07-12 11:28:32 +0200

    adding the home page

---
+++ view/home_page.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="A tool to test your Pydio or a new one" />
+    <title>Welcome to Pydio-test !!</title>
+    <?php include "media.html" ?>
+</head>
+<body>
+    <div class="container" id="the_body">
+    </div>
+</div>
+</body>
+</html>
