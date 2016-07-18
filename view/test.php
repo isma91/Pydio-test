7ee12a237c2ec2d5b984f328b9283b7b60ec51c9
isma91 <ismaydogmus@gmail.com>
2016-07-18 13:29:43 +0200

    adding test page

---
+++ view/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="A tool to test your Pydio or a new one" />
+    <title>Welcome to Pydio-test !!</title>
+    <?php include "media.html" ?>
+    <script src="media/js/test.js"></script>
+</head>
+<body>
+    <div class="container" id="the_body">
+        <div class="row mui-panel">
+            <h1 class="title">Welcome to Pydio-test !!</h1>
+            <h2 class="title">You selected the Workspace !!</h2>
+        </div>
+        <div class="row end_button">
+            <button class="waves-effect btn-flat" id="logout">Logout</button>
+        </div>
+    </div>
+</body>
+</html>
